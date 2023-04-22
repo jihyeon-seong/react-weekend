15728fcee955b416f42b50b70c5025051cc3b36b
Jihyeon Seong <dev.seongjihyeon@gmail.com>
2023-04-22 12:14:12 +0900

    ~점심시간 전까지. 컴포넌트

---
+++ Labs/lab04/src/components/MyCompo.js
@@ -0,0 +1,15 @@
+// 함수형 컴포넌트
+// function MyCompo() {
+//   return <div>MyCompo</div>;
+// }
+
+// 화살표 함수
+const MyCompo = () => {
+  return (
+    <>
+      <h1>Hello, My Component!</h1>
+    </>
+  );
+};
+
+export default MyCompo;
