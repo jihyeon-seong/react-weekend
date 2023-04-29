a243328cf284d00f42887d9ef95a508c5d6b1ddb
Jihyeon Seong <dev.seongjihyeon@gmail.com>
2023-04-29 15:30:14 +0900

    init lab05

--- Labs/lab04/src/components/MyEvent3.js
+++ Labs/lab04/src/components/MyEvent3.js
@@ -3,9 +3,14 @@ import React, { useCallback, useState } from 'react';
 // 함수형 컴포넌트 - state 초기값을 객체로 사용하기
 
 const MyEvent3 = () => {
-  const [form, setForm] = useState({ username: '', message: '' });
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
 
-  const handleChange = useCallback(e => {
+  // useCallback 에 관해 참고 문서 : https://www.freecodecamp.org/news/better-react-performance-usecallback-vs-usememo/
+  // One of the first things to note is precisely the function definition part. It stores the definition, not the execution itself, not the result – so the function will be executed every time it is called. Because of this, don't use this hook to avoid lengthy calculations.
+  const handleChange = e => {
     const { name, value } = e.target;
     // setForm({ [name]: value }); // Warning
     // setForm({ ...form, [name]: value }); // Warning 사라짐
@@ -14,7 +19,7 @@ const MyEvent3 = () => {
       ...prevState,
       [name]: value,
     }));
-  }, []);
+  };
 
   const clear = () => {
     setForm({ username: '', message: '' });
