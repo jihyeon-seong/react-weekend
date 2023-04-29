1b3e05ce2c262490516be22172e12ee35ac8d228
Jihyeon Seong <dev.seongjihyeon@gmail.com>
2023-04-29 14:10:43 +0900

    클래스형 컴포넌트에서 두 개 이상의 state 관리 & 이벤트 처리2

--- Labs/lab04/src/components/MyEvent.js
+++ Labs/lab04/src/components/MyEvent.js
@@ -12,6 +12,13 @@ export default class MyEvent extends Component {
 
     this.handleChangeUsername = this.handleChangeUsername.bind(this);
     this.handleChangeMessage = this.handleChangeMessage.bind(this);
+
+    // 위의 두 function을 하나의 function으로 만듦
+    this.handleChange = this.handleChange.bind(this);
+
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+
+    this.clear = this.clear.bind(this);
   }
 
   handleChangeUsername = e => {
@@ -22,6 +29,34 @@ export default class MyEvent extends Component {
 
   handleChangeMessage = e => {
     this.setState({ message: e.target.value });
+  };
+
+  // 위의 두 function을 하나의 function으로 만듦
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }); // e.target.name에 저장된 값을 key값으로 쓰고, e.target.value를 키의 값으로 씀
+  };
+
+  handleKeyDown = e => {
+    const { name, value } = e.target;
+    if (e.key === 'Enter') {
+      alert(`${name} : ${value}`);
+    }
+  };
+
+  clear = e => {
+    if (e.target.className === 'username') {
+      this.setState({ username: '' });
+    } else if (e.target.className === 'message') {
+      this.setState({ message: '' });
+    } else if (e.target.className === 'all') {
+      this.setState({ username: '' });
+      this.setState({ message: '' });
+
+      document.querySelectorAll('input[type="text"]').forEach(input => {
+        input.value = '';
+      });
+    }
   };
 
   render() {
@@ -36,7 +71,10 @@ export default class MyEvent extends Component {
             <input
               type="text"
               name="username"
-              onChange={this.handleChangeUsername}
+              //   onChange={this.handleChangeUsername}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyDown}
+              onClick={this.clear}
             />
             {/* username에 입력한 값들이 아래 readOnly input에 자동 업데이트 */}
             <input
@@ -45,6 +83,9 @@ export default class MyEvent extends Component {
               readOnly
               style={{ backgroundColor: 'lightgray' }}
             />
+            <button className="username" onClick={this.clear}>
+              Clear Username
+            </button>
           </div>
 
           <div>
@@ -52,7 +93,10 @@ export default class MyEvent extends Component {
             <input
               type="text"
               name="message"
-              onChange={this.handleChangeMessage}
+              //   onChange={this.handleChangeMessage}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyDown}
+              onClick={this.clear}
             />
             {/* message 입력한 값들이 아래 readOnly input에 자동 업데이트 */}
             <input
@@ -61,6 +105,15 @@ export default class MyEvent extends Component {
               readOnly
               style={{ backgroundColor: 'lightgray' }}
             />
+            <button className="message" onClick={this.clear}>
+              Clear Message
+            </button>
+          </div>
+
+          <div>
+            <button className="all" onClick={this.clear}>
+              Clear All
+            </button>
           </div>
         </div>
       </div>
